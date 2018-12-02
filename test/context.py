ee7add0110c03441beaf497c5fd870d4783a783d
Rahul Chatterjee <rahul@cs.cornell.edu>
2018-12-02 13:51:43 -0500

    Adding only phpbb and rockyou data

--- test/context.py
+++ test/context.py
@@ -9,7 +9,7 @@ from os.path import join
 from pwmodel.helper import thisdir
 from pwmodel.fast_fuzzysearch import Fast2FuzzySearch, lvdistance
 
-ry_leak_file = join(thisdir, 'data', 'rockyou-withcount.txt.gz')
+ry_leak_file = join(thisdir, 'data', 'rockyou_2M-withcount.txt.gz')
 phpbb_leak_file = join(thisdir, 'data', 'phpbb-withcount.txt.gz')
 # ry_leak_file = os.path.expanduser('~/passwords/rockyou-withcount.txt.gz')
 # phpbb_leak_file = os.path.expanduser('~/passwords/phpbb-withcount.txt.gz')
